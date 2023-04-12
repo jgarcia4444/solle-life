82919a5028466f8cd8358787b3c536a7285fa451
Jake Garcia <bballwiz4@gmail.com>
2023-04-11 19:36:37 -0700

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+
+    return (
+        <header className="w-full h-96 p-8">
+            <h1 className="text-4xl text-left">Solle Life</h1>
+        </header>
+    )
+};
+
+export default Header;
