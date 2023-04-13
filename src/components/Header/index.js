c9f71edc12372c43cf16c3160367fe09bc90578b
Jake Garcia <bballwiz4@gmail.com>
2023-04-12 17:08:42 -0700

    Added in content and an image as well as layout for desktop screen size in the header component.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,8 +3,14 @@ import React from 'react'
 const Header = () => {
 
     return (
-        <header className="w-full h-96 p-8">
-            <h1 className="text-4xl text-left">Solle Life</h1>
+        <header className="w-full p-8 flex flex-row">
+            <div className="lg:w-2/5 flex flex-col justify-evenly">
+                <h1 className="text-8xl text-left text-white font-bold">Solle Life</h1>
+                <p className="text-left text-white text-2xl">Natural supplements for almost every need</p>
+            </div>
+            <div className="lg:w-3/5 flex flex-col justify-center items-center">
+                <img className="lg:w-3/4 rounded shadow hover:shadow-lg" src="https://cdn11.bigcommerce.com/s-zsd73psd0i/product_images/uploaded_images/herbal-supplement-guide2.jpg" />
+            </div>
         </header>
     )
 };
