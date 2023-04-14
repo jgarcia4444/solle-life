1175205002083693c294f4272719a330c553bc06
Jake Garcia <bballwiz4@gmail.com>
2023-04-13 22:53:43 -0700

    Made the nav bar appear as a fixed nav bar once the user scrolls down.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,10 @@
 
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 
 const Navbar = () => {
+
+    const [showFixedNav, setShowFixedNav] = useState(false);
+    const [lastScrollY, setLastScrollY] = useState(0);
 
     const renderNavLinks = () => {
         let navObjects = [
@@ -13,8 +16,29 @@ const Navbar = () => {
         return navObjects.map((navObject, i) => <a key={`${i}-${navObject.text}`} className="text-lg mr-16 text-white hover:text-lime-600 hover:animate-bounce active:animate-none" href={navObject.to}>{navObject.text}</a>)
     }
 
+    const controlNavbar = () => {
+        if (typeof window !== 'undefined') { 
+            console.log(window.scrollY);
+          if (window.scrollY > 14) {
+            setShowFixedNav(true); 
+          } else if (window.scrollY < 15) { 
+            setShowFixedNav(false);  
+          }
+          setLastScrollY(window.scrollY); 
+        }
+      };
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            window.addEventListener('scroll', controlNavbar);
+            return () => {
+                window.removeEventListener('scroll', controlNavbar);
+            };
+        }
+    },[lastScrollY]);
+
     return (
-        <div className="w-full flex flex-row">
+        <div className={`w-full flex flex-row transition-all ${showFixedNav === true && "bg-lime-600 fixed top-0 left-0 p-2 shadow"}`}>
             <div className="w-1/4 flex flex-row items-center justify-start">
                 <h4 className="text-white font-bold text-4xl"><a href="#">Solle Life</a></h4>
             </div>
