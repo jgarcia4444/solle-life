776e6ccde0e051f946bfd4c71ed83f0c5c694328
Jake Garcia <bballwiz4@gmail.com>
2023-04-18 17:29:50 -0700

    Added in 3 images for the min body health tab.

--- src/components/Health/index.js
+++ src/components/Health/index.js
@@ -7,7 +7,7 @@ import healthCardImages from '../../config/healthCardImages';
 
 const Health = () => {
 
-    const {ashwagandha, yerbaMatte, holyBasil} = healthCardImages;
+    const {ashwagandha, yerbaMatte, holyBasil, detox, reindeerLichen, quality,} = healthCardImages;
 
     const [selectedTabIndex, setSelectedTabIndex] = useState(0);
 
@@ -15,9 +15,9 @@ const Health = () => {
         {tabIndex: 0, title: "Ashwaganda the Powerful Adaptogen", text: "Ashwagandha is an ancient medicinal herb that has been used for centuries in Ayurvedic medicine, which is the traditional system of medicine in India that has been traced back to 6,000 BC. ", img: ashwagandha},
         {tabIndex: 0, title: "Yerba Matte - 5 Reasons to Take This Adaptogen", text: "Yerba mate is a popular beverage in South and Central America, made from the leaves and stems of the Ilex paraguariensis plant. It has been used for centuries but is becoming increasingly popular due to its amazing health benefits.", img: yerbaMatte},
         {tabIndex: 0, title: "Holy Basil - A Powerful Adaptogen", text: "Holy basil or tulsi, also known as Ocimum tenuiflorum or Ocimum sanctum, is an aromatic shrub in the Lamiaceae basil plant family.", img: holyBasil},
-        {tabIndex: 1, title: "Benefits of a detox cleanse", text: "Our bodies can use a little bit of a deep cleanse every now and again as well. Most people in the health world call this a detox cleanse.", img: ""},
-        {tabIndex: 1, title: "Reindeer Lichen - A Plant Source for Vitamin D", text: "It helps support our bones and muscles, keep our immune system healthy, regulate hormones, and even protect against certain diseases.", img: ""},
-        {tabIndex: 1, title: "Our Dedication to Quality", text: "Here at Solle Naturals, we want to provide you with the highest quality products available. Each raw ingredient we receive is tested to make sure it has everything we expect and has nothing we don't.", img: ""},
+        {tabIndex: 1, title: "Benefits of a detox cleanse", text: "Our bodies can use a little bit of a deep cleanse every now and again as well. Most people in the health world call this a detox cleanse.", img: detox},
+        {tabIndex: 1, title: "Reindeer Lichen - A Plant Source for Vitamin D", text: "It helps support our bones and muscles, keep our immune system healthy, regulate hormones, and even protect against certain diseases.", img: reindeerLichen},
+        {tabIndex: 1, title: "Our Dedication to Quality", text: "Here at Solle Naturals, we want to provide you with the highest quality products available. Each raw ingredient we receive is tested to make sure it has everything we expect and has nothing we don't.", img: quality},
         {tabIndex: 2, title: "The Power of Body Language", text: "Do you ever catch yourself slouching? Or maybe catch yourself making a really funny face? Or maybe even catch yourself making a fist when you feel angry?", img: ""},
         {tabIndex: 2, title: "Overcoming Barriers to Success", text: "There are any number of people who have already created success, both in terms of business and in terms of self-care and health. They share what worked for them over and over in a generous way.", img: ""},
         {tabIndex: 2, title: "Setting and Achieving Goals", text: "If you were to walk up to any person on the street and ask them if they have goals, almost everyone would certainly say yes. In fact, if you were to inquire about what these goals are, many would probably list off similar objectives:", img: ""},
--- src/config/healthCardImages.js
+++ src/config/healthCardImages.js
@@ -2,9 +2,15 @@
 import ashwagandha from '../media/adaptogens/ashwagandha.jpeg';
 import yerbaMatte from '../media/adaptogens/yerba-matte.jpeg';
 import holyBasil from '../media/adaptogens/holy-basil.webp';
+import detox from '../media/mindBodyHealth/detox.jpeg';
+import quality from '../media/mindBodyHealth/quality.jpeg';
+import reindeerLichen from '../media/mindBodyHealth/reindeer-lichen.jpeg';
 
 export default {
     ashwagandha,
     yerbaMatte,
     holyBasil,
+    detox,
+    reindeerLichen,
+    quality,
 }
