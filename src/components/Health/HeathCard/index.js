6cd0a682f1103d6b6560361b57b994f9d2788a2e
Jake Garcia <bballwiz4@gmail.com>
2023-04-30 16:31:40 -0700

    Health tab columns now fade and scale into view when presented.

--- src/components/Health/HeathCard/index.js
+++ src/components/Health/HeathCard/index.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
 
 const HealthCard = ({cardInfo}) => {
 
     const {img, title, text} = cardInfo;
 
+    const [fade, setFade] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setFade(true);
+        }, 50)   
+    })
+
     return (
-        <div className="lg:w-1/4 lg:m-4 flex lg:flex-col justify-start items-center">
+        <div className={`lg:w-1/4 lg:m-4 flex lg:flex-col justify-start items-center ${fade === true ? "opacity-100 scale-100" : "scale-0 opacity-0"} transition-all duration-500`}>
             <img src={img} alt="Health Card" className="w-full h-44 rounded" />
             <h6 className="font-bold text-2xl text-left text-white">{title}</h6>
             <p className="text-lg text-left mt-2 text-white">{text}</p>
